15fa47da3bedc9f1a8c4fa68f4eed7e76fdec195
Bjoern Kasper <urmel@github.com>
2022-02-15 22:05:30 +0100

    added functions for measuring temperature to class 'PyVisa_Keysight_34465A'

--- PyVisa_Keysight_34465A_class.py
+++ PyVisa_Keysight_34465A_class.py
@@ -7,6 +7,14 @@ Wrapper class to communicate with the DMM Keysight 34465A via LAN interface and 
 """
 import pyvisa
 import time, sys
+from enum import Enum
+
+class MeasType(Enum):
+    VOLT = "VOLT"
+    CURR = "CURR"
+    RES = "RES"
+    TEMP = "TEMP"
+    FREQ = "FREQ"
 
 class PyVisa_Keysight_34465A():
     def __init__(self, tcp_ip):
@@ -75,227 +83,41 @@ class PyVisa_Keysight_34465A():
             self.status = "Error"
             print("Device is not connected")
     
-    # #define a TOGGLE OUTPUT function
-    # def toggleOutput(self, chan, state):
-    #     if (self.status == "Connected"):
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             if ((state == 'ON') or (state == 'OFF')):
-    #                 self.cmd1 = ':OUTP CH%s,%s' %(chan, state)
-    #                 self.psu.write(self.cmd1)
-    #                 time.sleep(self._delay)
-    #                 return chan, state
-    #             else:
-    #                 print("Wrong state provided <'ON'|'OFF'>")
-    #                 return -1
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
-    
-    # #define a SET VOLTAGE function
-    # def setVoltage(self, chan, voltage):
-    #     if (self.status == "Connected"):
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             # check voltage limits
-    #             if (voltage < self.VOLTAGE_MIN): voltage = self.VOLTAGE_MIN
-    #             if ((chan == 1) or (chan == 2)):
-    #                 if (voltage > self.VOLTAGE_MAX_1_2): voltage = self.VOLTAGE_MAX_1_2
-    #             elif (chan == 3):
-    #                 if (voltage > self.VOLTAGE_MAX_3): voltage = self.VOLTAGE_MAX_3
-                
-    #             self.chan_curr = self.psu.query(':INST:NSEL?')
-    #             if (self.chan_curr != chan):
-    #                 self.cmd1 = ':INST:NSEL %s' %chan
-                
-    #             self.cmd2 = ':VOLT %s' %voltage
-    #             self.psu.write(self.cmd1)
-    #             time.sleep(self._delay)
-    #             self.psu.write(self.cmd2)
-    #             time.sleep(self._delay)
-    #             return chan, voltage
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
-        
-    # #define a SET CURRENT function
-    # def setCurrent(self, chan, current):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             # check current limits
-    #             if (current < self.CURRENT_MIN): current = self.CURRENT_MIN
-    #             if (current > self.CURRENT_MAX): current = self.CURRENT_MAX
-                
-    #             self.chan_curr = self.psu.query(':INST:NSEL?')
-    #             if (self.chan_curr != chan):
-    #                 self.cmd1 = ':INST:NSEL %s' %chan
-                
-    #             self.cmd2 = ':CURR %s' %current
-    #             self.psu.write(self.cmd1)
-    #             time.sleep(self._delay)
-    #             self.psu.write(self.cmd2)
-    #             time.sleep(self._delay)
-    #             return chan, current
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
-        
-    # #define a SET OVERVOLTAGE PROTECTION function
-    # def setOVP(self, chan, ovp):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             # check ovp limits
-    #             if (ovp < self.OVP_MIN): ovp = self.OVP_MIN
-    #             if ((chan == 1) or (chan == 2)):
-    #                 if (ovp > self.OVP_MAX_1_2): ovp = self.OVP_MAX_1_2
-    #             elif (chan == 3):
-    #                 if (ovp > self.OVP_MAX_3): ovp = self.OVP_MAX_3
-                
-    #             self.chan_curr = self.psu.query(':INST:NSEL?')
-    #             if (self.chan_curr != chan):
-    #                 self.cmd1 = ':INST:NSEL %s' %chan
+    #define a CONFigure TEMPerature MEASUREment function
+    def confTempMeasure(self):
+        if (self.status != "Connected"):
+            print("Device is not connected")
+            return -1
 
-    #             self.cmd2 = ':VOLT:PROT %s' %ovp
-    #             self.psu.write(self.cmd1)
-    #             time.sleep(self._delay)
-    #             self.psu.write(self.cmd2)
-    #             time.sleep(self._delay)
-    #             return chan, ovp
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
+        # reset device
+        self.cmd = '*RST'
+        self.psu.write(self.cmd)
+        time.sleep(self._delay)
 
-    # #define a TOGGLE OVERVOLTAGE PROTECTION function
-    # def toggleOVP(self, chan, state):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             if ((state == 'ON') or (state == 'OFF')):
-    #                 self.chan_curr = self.psu.query(':INST:NSEL?')
-    #                 if (self.chan_curr != chan):
-    #                     self.cmd1 = ':INST:NSEL %s' %chan
-                    
-    #                 self.cmd2 = ':VOLT:PROT:STAT %s' %state
-    #                 self.psu.write(self.cmd1)
-    #                 time.sleep(self._delay)
-    #                 self.psu.write(self.cmd2)
-    #                 time.sleep(self._delay)
-    #                 return chan, state
-    #             else:
-    #                 print("Wrong state provided <'ON'|'OFF'>")
-    #                 return -1
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
+        # configure measurement for 4-wire RTD (here an PT100)
+        self.cmd = 'CONF:TEMP FRTD,85,1,0.0000001'
+        self.psu.write(self.cmd)
+        time.sleep(self._delay)
 
-    # #define a SET OVERCURRENT PROTECTION function
-    # def setOCP(self, chan, ocp):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             # check ocp limits
-    #             if (ocp < self.OCP_MIN): ocp = self.OCP_MIN
-    #             if (ocp > self.OCP_MAX): ocp = self.OCP_MAX
-                
-    #             self.chan_curr = self.psu.query(':INST:NSEL?')
-    #             if (self.chan_curr != chan):
-    #                 self.cmd1 = ':INST:NSEL %s' %chan
-                
-    #             self.cmd2 = ':CURR:PROT %s' %ocp
-    #             self.psu.write(self.cmd1)
-    #             time.sleep(self._delay)
-    #             self.psu.write(self.cmd2)
-    #             time.sleep(self._delay)
-    #             return chan, ocp
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
+        # select unit °C to be used for all temperature measurements
+        self.cmd = 'UNIT:TEMP C'
+        self.psu.write(self.cmd)
+        time.sleep(self._delay)
 
-    # #define a TOGGLE OVERCURRENT PROTECTION function
-    # def toggleOCP(self, chan, state):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             if ((state == 'ON') or (state == 'OFF')):
-    #                 self.chan_curr = self.psu.query(':INST:NSEL?')
-    #                 if (self.chan_curr != chan):
-    #                     self.cmd1 = ':INST:NSEL %s' %chan
-                    
-    #                 self.cmd2 = ':CURR:PROT:STAT %s' %state
-    #                 self.psu.write(self.cmd1)
-    #                 time.sleep(self._delay)
-    #                 self.psu.write(self.cmd2)
-    #                 time.sleep(self._delay)
-    #                 return chan, state
-    #             else:
-    #                 print("Wrong state provided <'ON'|'OFF'>")
-    #                 return -1
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
-        
-    # #define a MEASURE VOLTAGE function
-    # def measVolt(self, chan):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             self.cmd1 = ':MEAS:VOLT? CH%s' %chan
-    #             self.V = self.psu.query(self.cmd1)
-    #             self.V = float(self.V)
-    #             time.sleep(self._delay)
-    #             return chan, self.V
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
-    
-    # #define a MEASURE CURRENT function
-    # def measCurrent(self, chan):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             self.cmd1 = ':MEAS:CURR? CH%s' %chan
-    #             self.C = self.psu.query(self.cmd1)
-    #             self.C = float(self.C)
-    #             time.sleep(self._delay)
-    #             return chan, self.C
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
+    #define a GET MEASUREMENT function
+    def getMeasurement(self, measType):
+        if (self.status != "Connected"):
+            print("Device is not connected")
+            return -1
 
-    # #define a MEASURE POWER function
-    # def measPower(self, chan):
-    #     if self.status == "Connected":
-    #         if ((chan == 1) or (chan == 2) or (chan == 3)):
-    #             self.cmd1 = ':MEAS:POWE? CH%s' %chan
-    #             self.P = self.psu.query(self.cmd1)
-    #             self.P = float(self.P)
-    #             time.sleep(self._delay)
-    #             return chan, self.P
-    #         else:
-    #             print("Wrong channel selected <1|2|3>")
-    #             return -1
-    #     else:
-    #         print("Device is not connected")
-    #         return -1
+        # # check valid measurement type
+        # if not isinstance(measType, MeasType):
+        #     raise TypeError("Parameter 'measType' must be an instance of 'MeasType' Enum")
 
+        # trigger the measurement as single shot and retrieve 1 dataset
+        self.cmd = 'MEAS:%s?' %measType.value
+        self.ret_val = self.psu.query(self.cmd)
+        self.ret_val = float(self.ret_val)
+        time.sleep(self._delay)
 
+        return self.ret_val
